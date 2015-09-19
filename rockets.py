7d590a43f0cc6c7033bf01c9973e306bf4ff7d54
brupoon <brupoon@outlook.com>
2015-09-19 10:06:33 -0400

    changed to radians

--- rockets.py
+++ rockets.py
@@ -1,7 +1,11 @@
 import numpy as np
 from scipy import stats
 data = np.loadtxt("rockets.txt")
-col = data[:]
-print("mean: ", np.mean(col))
-print("stdev: ", stats.tstd(col))
-print("error of mean: ", stats.sem(col))
+col, cold = data[:], []
+for item in range(len(col)):
+	cold.append(np.radians(col[item]))
+print("""
+mean: {0}
+stdev: {1}
+error of mean: {2}
+	""".format(np.mean(cold),stats.tstd(cold),stats.sem(cold)))
