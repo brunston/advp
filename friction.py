4dce61967bc6557411ea1762849077f3b596d872
brupoon <brupoon@outlook.com>
2015-11-16 21:02:16 -0500

    changes to friction.py

--- friction.py
+++ friction.py
@@ -1,13 +1,10 @@
 #Brunston Poon (c) 2015, MIT License
 import numpy as np
-from scipy import stats
 import math
 
 muk = [0.2364, 0.2199, 0.2313]
 std_muk = np.std(muk)
-
-print("stdev of muk:", std_muk)
-
 error_prop_to_theta = math.sqrt((1/((std_muk**2)+1))**2)
 
+print("stdev of muk:", std_muk)
 print("error propagation to theta:", error_prop_to_theta)
