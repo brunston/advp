0ac442ec8ca9b95ed3f4800875b995605e9bd99a
brupoon <brupoon@outlook.com>
2015-11-17 07:53:23 -0500

    proper propagation

--- friction.py
+++ friction.py
@@ -4,7 +4,10 @@ import math
 
 muk = [0.2364, 0.2199, 0.2313]
 std_muk = np.std(muk)
-error_prop_to_theta = math.sqrt((1/((std_muk**2)+1))**2)
+avg_muk = np.mean(muk)
+error_prop_to_theta = math.sqrt((1/((avg_muk**2)+1))*std_muk)
 
 print("stdev of muk:", std_muk)
+print("avg of muk:", avg_muk)
 print("error propagation to theta:", error_prop_to_theta)
+print("error prop in degrees:", error_prop_to_theta * 180 / math.pi)
