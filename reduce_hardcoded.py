cfbcb8b8001ab6c3703f2a0f82da8551d2fd4c6a
Brunston Poon <albatrossian@outlook.com>
2016-05-10 13:20:50 -0400

    these matrices don't work

--- reduce_hardcoded.py
+++ reduce_hardcoded.py
@@ -3,7 +3,24 @@
 from sympy import *
 init_printing(use_unicode=True)
 from sympy.abc import a,r
+i1,i2,i3,i4,i5=symbols('i1,i2,i3,i4,i5')
 
-A = Matrix([[1,2,0,0,0,a],[0,0,0,4,5,a],[0,2,3,4,0,a],[1,0,3,0,5,a],[0,2,3,0,5,0]])
-print("A: ",A)
-print("A.rref()",A.rref())
+A = Matrix([[0,0,0,4*r,5*r,a],
+            [r,2*r,0,0,0,a],
+            [-r,0,3*r,4*r,0,0],
+            [0,-2*r,3*r,0,5*r,0],
+            [0,2*r,3*r,4*r,0,a],
+            [r,-3*r,0,0,5,a]])
+
+B = Matrix([[0,0,0,4*r,5*r,a],
+            [r,2*r,0,0,0,a],
+            [1,-1,1,0,0,0],
+            [0,0,-1,1,-1,0],
+            [0,2*r,3*r,4*r,0,a],
+            [r,-3*r,0,0,5,a]])
+
+print("A: \n",A)
+print("rref of A (augmented):\n", solve_linear_system(A,i1,i2,i3,i4,i5))
+
+print("B: \n",B)
+print("rref of B (augmented):\n", solve_linear_system(B,i1,i2,i3,i4,i5))
